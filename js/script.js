c67a6aeaeff1627d2e370eff3e18a2249ead8881
hitmaizer <jose.sousalves@gmail.com>
2022-02-20 16:00:51 +0000

    user-profile-work

---
+++ js/script.js
@@ -0,0 +1,118 @@
+function statusHandler() {
+    const statusToggle = document.getElementById('status_toggle')
+    const statusDiv = document.getElementById('status')
+    const groupElements = document.querySelectorAll('.show')
+
+    if (statusToggle.classList.contains('show')) {
+        statusToggle.classList.remove('show')
+    } else {
+        groupElements.forEach(e => {
+            if(e.classList.contains('show')) {
+                e.classList.remove('show')
+        }
+    })
+    statusDiv.classList.add('show')
+    }
+}
+
+function propostaHandler() {
+    const propostaToggle = document.getElementById('proposta_toggle')
+    const propostaDiv = document.getElementById('proposta')
+    const groupElements = document.querySelectorAll('.show')
+
+    if (propostaToggle.classList.contains('show')) {
+        propostaToggle.classList.remove('show')
+    } else {
+        groupElements.forEach(e => {
+            if(e.classList.contains('show')) {
+                e.classList.remove('show')
+        }
+    })
+    propostaDiv.classList.add('show')
+    }
+}
+
+function orcamentoHandler() {
+    const orcamentoToggle = document.getElementById('orcamento_toggle')
+    const orcamentoDiv = document.getElementById('orcamento')
+    const groupElements = document.querySelectorAll('.show')
+
+    if (orcamentoToggle.classList.contains('show')) {
+        orcamentoToggle.classList.remove('show')
+    } else {
+        groupElements.forEach(e => {
+            if(e.classList.contains('show')) {
+                e.classList.remove('show')
+        }
+    })
+    orcamentoDiv.classList.add('show')
+    }
+}
+
+function fase1Handler() {
+    const fase1Toggle = document.getElementById('fase1_toggle')
+    const fase1Div = document.getElementById('fase1')
+    const groupElements = document.querySelectorAll('.show')
+
+    if (fase1Toggle.classList.contains('show')) {
+        fase1Toggle.classList.remove('show')
+    } else {
+        groupElements.forEach(e => {
+            if(e.classList.contains('show')) {
+                e.classList.remove('show')
+        }
+    })
+    fase1Div.classList.add('show')
+    }
+}
+
+function fase2Handler() {
+    const fase2Toggle = document.getElementById('fase2_toggle')
+    const fase2Div = document.getElementById('fase2')
+    const groupElements = document.querySelectorAll('.show')
+
+    if (fase2Toggle.classList.contains('show')) {
+        fase2Toggle.classList.remove('show')
+    } else {
+        groupElements.forEach(e => {
+            if(e.classList.contains('show')) {
+                e.classList.remove('show')
+        }
+    })
+    fase2Div.classList.add('show')
+    }
+}
+
+function fase3Handler() {
+    const fase3Toggle = document.getElementById('fase3_toggle')
+    const fase3Div = document.getElementById('fase3')
+    const groupElements = document.querySelectorAll('.show')
+
+    if (fase3Toggle.classList.contains('show')) {
+        fase3Toggle.classList.remove('show')
+    } else {
+        groupElements.forEach(e => {
+            if(e.classList.contains('show')) {
+                e.classList.remove('show')
+        }
+    })
+    fase3Div.classList.add('show')
+    }
+}
+
+function finalHandler() {
+    const finalToggle = document.getElementById('final_toggle')
+    const finalDiv = document.getElementById('final')
+    const groupElements = document.querySelectorAll('.show')
+
+    if (finalToggle.classList.contains('show')) {
+        finalToggle.classList.remove('show')
+    } else {
+        groupElements.forEach(e => {
+            if(e.classList.contains('show')) {
+                e.classList.remove('show')
+        }
+    })
+    finalDiv.classList.add('show')
+    }
+}
---
+++ template-parts/user-profile.php
@@ -0,0 +1,177 @@
+<?php
+/* Template Name: User Profile */
+
+get_header();
+$username = wp_get_current_user()->user_firstname;
+$current_user = wp_get_current_user();
+$status = get_field("status", "user_" . $current_user->ID);
+$proposta = have_rows("proposta");
+$orcamento = have_rows("orcamento");
+$fase1 = have_rows("fase_1");
+$final_files = have_rows("final_files");
+
+print_r($status)
+
+?>
+
+<!-- AQUI VAI O TEU HTML -->
+
+    <div class="page__wrapper flex-row">
+        <div class="page__sidebar flex-col">
+            <ul class="sidebar__menu flex-col">
+                <div class="list__item flex-row" id="status_toggle" onclick="statusHandler()">
+                    <li>Estado</li>
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/images/arrow.png" alt="" class="item__icon">
+                </div>
+                <div class="list__item flex-row" id="proposta_toggle" onclick="propostaHandler()">
+                    <li>Proposta</li>
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/images/arrow.png" alt="" class="item__icon">
+                </div>
+                <div class="list__item flex-row" id="orcamento_toggle" onclick="orcamentoHandler()">
+                    <li>Orçamento</li>
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/images/arrow.png" alt="" class="item__icon">
+                </div>
+                <div class="list__item flex-row" id="fase1_toggle" onclick="fase1Handler()">
+                    <li>Fase 1</li>
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/images/arrow.png" alt="" class="item__icon">
+                </div>
+                <div class="list__item flex-row" id="fase2_toggle" onclick="fase2Handler()">
+                    <li>Fase 2</li>
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/images/arrow.png" alt="" class="item__icon">
+                </div>
+                <div class="list__item flex-row" id="fase3_toggle" onclick="fase3Handler()">
+                    <li>Fase 3</li>
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/images/arrow.png" alt="" class="item__icon">
+                </div>
+                <div class="list__item flex-row" id="final_toggle" onclick="finalHandler()">
+                    <li>Ficheiros Finais</li>
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/images/arrow.png" alt="" class="item__icon">
+                </div>
+            </ul>
+        </div>
+        <div class="page__content flex-col">
+
+            <h1 class="welcome__header">Bem-vindo <?php echo $username ?>.</h1>
+
+            <div class="status__container container" id="status">
+                <h1 class="container__header status__header">O seu projecto encontra-se na <span class="strong__txt"><?php echo $status["label"]?></span>.</h1>
+                <?php 
+                    switch($status["value"]) {
+                        case "proposta": 
+                            ?>
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/status1.png" alt="" class="status__img">
+                        <?php
+                        break;
+                        case "orcamento": 
+                            ?>
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/status2.png" alt="" class="status__img">
+                        <?php
+                        break;
+                        case "fase1": 
+                            ?>
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/status3.png" alt="" class="status__img">
+                        <?php
+                        break;
+                        case "finalfiles": 
+                            ?>
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/status.png" alt="" class="status__img">
+                        <?php
+                        break;
+                        default : 
+                            ?>
+                        <h1>O seu projecto ainda vai ser iniciado.</h1>
+                        <?php
+                        break;
+
+                    }
+                ?>
+            </div>
+
+            <div class="proposta__container container" id="proposta">
+                <h1 class="container__header proposta__header">Aqui pode consultar a proposta para o seu projecto.</h1>
+                <div class="files__grid">
+                    <div class="file__container">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/file.png" alt="" class="file__placeholder">
+                        <p class="file__description">Design.pdf</p>
+                    </div>
+                    <div class="file__container">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/file.png" alt="" class="file__placeholder">
+                        <p class="file__description">Video.pdf</p>
+                    </div>
+                    <div class="file__container">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/file.png" alt="" class="file__placeholder">
+                        <p class="file__description">WebDesign.pdf</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="orcamento__container container" id="orcamento">
+                <h1 class="container__header orcamento__header">Aqui pode consultar o orçamento do seu projecto.</h1>
+                <div class="file__container">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/file.png" alt="" class="file__placeholder">
+                        <p class="file__description">Orçamento.pdf</p>
+                </div>
+            </div>
+
+            <div class="fase1__container" id="fase1">
+                <h1 class="container__header fase__header">Aqui pode consultar a Fase 1 do seu Projecto.</h1>
+                <div class="file__container">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/file.png" alt="" class="file__placeholder">
+                        <p class="file__description">Fase1.pdf</p>
+                </div>
+            </div>
+            <div class="fase2__container" id="fase2">
+                <h1 class="container__header fase__header">Aqui pode consultar a Fase 2 do seu Projecto.</h1>
+                <div class="file__container">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/file.png" alt="" class="file__placeholder">
+                        <p class="file__description">Fase2.pdf</p>
+                </div>
+            </div>
+            <div class="fase3__container" id="fase3">
+                <h1 class="container__header fase__header">Aqui pode consultar a Fase 3 do seu Projecto.</h1>
+                <div class="file__container">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/file.png" alt="" class="file__placeholder">
+                        <p class="file__description">Fase3.pdf</p>
+                </div>
+            </div>
+
+            <div class="final__container" id="final">
+                <h1 class="container__header final__header">Aqui pode consultar os ficheiros finais do seu projecto</h1>
+                <div class="files__grid final__files">
+                    <div class="file__container">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/file.png" alt="" class="file__placeholder">
+                        <p class="file__description">CartaoDeVisita.pdf</p>
+                    </div>
+                    <div class="file__container">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/file.png" alt="" class="file__placeholder">
+                        <p class="file__description">Item1.pdf</p>
+                    </div>
+                    <div class="file__container">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/file.png" alt="" class="file__placeholder">
+                        <p class="file__description">Item2.pdf</p>
+                    </div>
+                    <div class="file__container">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/file.png" alt="" class="file__placeholder">
+                        <p class="file__description">Video.mov</p>
+                    </div>
+                    <div class="file__container">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/file.png" alt="" class="file__placeholder">
+                        <p class="file__description">Website</p>
+                    </div>
+                    <div class="file__container">
+                        <img src="<?php echo get_template_directory_uri() ?>/assets/images/file.png" alt="" class="file__placeholder">
+                        <p class="file__description">Logotipo.ai</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    
+
+
+
+<?php
+    get_footer();
+?>
+
