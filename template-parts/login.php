1391c6f3e881e30c1f13e955c29cb668dde79026
hitmaizer <jose.sousalves@gmail.com>
2022-02-21 10:04:29 +0000

    basic login markup + redirect to userprofile

--- template-parts/login.php
+++ template-parts/login.php
@@ -2,23 +2,33 @@
     /* Template Name: Login Page */
     get_header();
 
-if ( ! is_user_logged_in() ) { // Display WordPress login form:
-    $args = array(
-        'redirect' => admin_url(), 
-        'form_id' => 'loginform-custom',
-        'label_username' => __( 'Username' ),
-        'label_password' => __( 'Password' ),
-        'label_remember' => __( 'Lembrar-me' ),
-        'label_log_in' => __( 'Entrar' ),
-        'remember' => true
-    );
-    wp_login_form( $args );
-} else { // If logged in:
-    wp_loginout( home_url() ); // Display "Log Out" link.
-    echo " | ";
-    wp_register('', ''); // Display "Site Admin" link.
-}
 ?>
 
+<div class="loginpage__wrapper">
+
+
+<div class="form__container">
+    <?php
+    if ( ! is_user_logged_in() ) { // Display WordPress login form:
+        $args = array(
+            'redirect' => home_url( '/user-profile/' ), 
+            'form_id' => 'loginform-custom',
+            'label_username' => __( 'Username' ),
+            'label_password' => __( 'Password' ),
+            'label_remember' => __( 'Lembrar-me' ),
+            'label_log_in' => __( 'Entrar' ),
+            'remember' => true
+        );
+        wp_login_form( $args );
+    } else { // If logged in:
+        wp_loginout( home_url() ); // Display "Log Out" link.
+        echo " | ";
+        wp_register('', ''); // Display "Site Admin" link.
+    }
+    ?>
+</div>
+
+
+</div>
 <?php 
 get_footer();
