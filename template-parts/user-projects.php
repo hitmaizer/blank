1d200e76589d6113d7f52d805555c352ccbaf2fd
hitmaizer <jose.sousalves@gmail.com>
2022-02-26 14:50:22 +0000

    html markup for projects page

--- template-parts/user-projects.php
+++ template-parts/user-projects.php
@@ -11,9 +11,20 @@ $status = get_field("status", "user_" . $current_user->ID);
 ?>
 
 <!--  Projects HTML -----> 
+<div class="projectspage__wrapper">
+    <div class="content__container flex-row">
+        <div class="menu__wrapper">
+            <h6 class="menu__header">Olá, <?php echo $username ?></h6>
+        </div>
+        <div class="content__wrapper">
+            <h1 class="projects__title">Bem-vindo, esta é a sua área. Escolha e aceda a todo o conteudo do projeto</h1>
+            <p class="placeholder__project">Titulo do projeto 1</p>
+            <p class="placeholder__project">Titulo do projeto 2</p>
+        </div>
+    </div>
 
 
-
+</div>
 
 <?php 
 get_footer();
