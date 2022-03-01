fb2ca1b255c6c2cb0ad866d1b7b906ae2c4b0a5c
Jose Alves <jose.sousalves@gmail.com>
2022-03-01 16:04:55 +0000

    added flex col to container

--- template-parts/login.php
+++ template-parts/login.php
@@ -6,9 +6,10 @@
 
 <div class="loginpage__wrapper">
 
-    <img src="<?php echo get_template_directory_uri() ?>/assets/images/logo-maionese.png" alt="" class="login__logo">
+    
 
-    <div class="form__container">
+    <div class="form__container flex-col">
+    <img src="<?php echo get_template_directory_uri() ?>/assets/images/logo-maionese.png" alt="" class="login__logo">
     <h1 class="login__header">Área de cliente</h1>
     <?php
     if ( ! is_user_logged_in() ) { // Display WordPress login form:
