61bce9a5b7c88a0026950a6ae8544637cb7f0239
Jose Alves <jose.sousalves@gmail.com>
2022-03-02 10:53:26 +0000

    sair btn to login page

--- template-parts/user-profile.php
+++ template-parts/user-profile.php
@@ -13,7 +13,9 @@ $index = (isset($_GET['id'])) ? $_GET['id'] : "0";
 <!-- AQUI VAI O TEU HTML -->
     <nav class="client__nav flex-row">
         <img src="<?php echo get_template_directory_uri() ?>/assets/images/logonav.png" alt="" class="nav__logo">
-        <button class="logout__btn">Sair</button>
+        <button class="logout__btn" >
+            <a href="/wordpress/login">Sair</a> 
+        </button>
         <img src="<?php echo get_template_directory_uri() ?>/assets/images/navbar-menu-mob.png" alt="" class="navbar__webmenu">
 
     </nav>
