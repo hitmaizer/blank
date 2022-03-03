7a0bb368493782f629ba91a90c0a05ee2b08ca04
Jose Alves <jose.sousalves@gmail.com>
2022-03-03 17:09:47 +0000

    footer markup adjusts

--- template-parts/user-profile.php
+++ template-parts/user-profile.php
@@ -761,11 +761,13 @@ $index = (isset($_GET['id'])) ? $_GET['id'] : "0";
         </div>
         <div class="footer__section flex-col">
             <h3 class="footer__title">Contactos</h3>
-            <div class="contactos__wrapper">
+            <div class="contactos__wrapper flex-col">
                 <a href="" class="footer__link">geral@maionesedesign.pt</a>
                 <a href="" class="footer__link">913 023 001</a>
-                <a href="" class="footer__link">Rua Alfredo Pereira 190
-                    4560-502 Penafiel</a>
+                <a href="" class="footer__link">
+                    <p class="footer__morada">Rua Alfredo Pereira 190</p> 
+                    <p class="footer__morada">4560-502 Penafiel</p> 
+                </a>
             </div>
         </div>
         <div class="footer__section flex-col">
