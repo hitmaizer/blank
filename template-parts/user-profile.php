267f0e9ac6e8fd6b0d47b1515e4ceaccb4121da4
Jose Alves <jose.sousalves@gmail.com>
2022-03-03 16:18:00 +0000

    fix typo + contact wrapper

--- template-parts/user-profile.php
+++ template-parts/user-profile.php
@@ -760,11 +760,13 @@ $index = (isset($_GET['id'])) ? $_GET['id'] : "0";
             <p class="footer__logotext">Todos os direitos reservados</p>
         </div>
         <div class="footer__section flex-col">
-            <h3 class="footer__title">Contatos</h3>
-            <a href="" class="footer__link">geral@maionesedesign.pt</a>
-            <a href="" class="footer__link">913 023 001</a>
-            <a href="" class="footer__link">Rua Alfredo Pereira 190
-                4560-502 Penafiel</a>
+            <h3 class="footer__title">Contactos</h3>
+            <div class="contactos__wrapper">
+                <a href="" class="footer__link">geral@maionesedesign.pt</a>
+                <a href="" class="footer__link">913 023 001</a>
+                <a href="" class="footer__link">Rua Alfredo Pereira 190
+                    4560-502 Penafiel</a>
+            </div>
         </div>
         <div class="footer__section flex-col">
             <h3 class="footer__title">Social</h3>
