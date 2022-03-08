ec1a1d186cf2daeec3232d026e63c7f9eae0aa5b
Jose Alves <jose.sousalves@gmail.com>
2022-03-08 17:49:41 +0000

    created new page for logout dialog

---
+++ template-parts/logout.php
@@ -0,0 +1,13 @@
+<?php
+    /* Template Name: Logout Page */
+    get_header();
+
+?>
+
+<div class="logout__wrapper">
+    <div class="dialog__container flex-col">
+        <img class="login__logo" src="<?php echo get_template_directory_uri() ?>/assets/images/logo-maionese.png" alt="" class="logout__img">
+    </div>
+</div>
+<?php 
+get_footer();
