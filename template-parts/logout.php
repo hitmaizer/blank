972454776245693aa968f05a3b3b00a2363d77e6
Jose Alves <jose.sousalves@gmail.com>
2022-03-08 18:01:28 +0000

    btn markup

--- template-parts/logout.php
+++ template-parts/logout.php
@@ -4,9 +4,15 @@
 
 ?>
 
-<div class="logout__wrapper">
+<div class="logout__wrapper flex-col">
     <div class="dialog__container flex-col">
         <img class="login__logo" src="<?php echo get_template_directory_uri() ?>/assets/images/logo-maionese.png" alt="" class="logout__img">
+        <h1 class="login__header">Até breve.</h1>
+    </div>
+    <div class="back__btn">
+        <a href="http://maionesedesign.pt" class="btn__link">
+            <h1 class="btn__text--back">maionesedesign.pt</h1>
+        </a>
     </div>
 </div>
 <?php 
