78a436574833c31daa049a9e1122e4f57dd721de
Jose Alves <jose.sousalves@gmail.com>
2022-03-10 12:33:22 +0000

    logo footer to link user projects

--- template-parts/user-projects.php
+++ template-parts/user-projects.php
@@ -54,7 +54,7 @@ $rows = get_field("projeto", "user_" . $current_user->ID);
     <div class="bottom__client flex-row">
         <div class="footer__section flex-col">
             <a href="http://maionesedesign.pt" class="footer__link">
-                <img src="<?php echo get_template_directory_uri() ?>/assets/images/logo-footer.png" alt="" class="footer__logo">
+                <img src="https://www.maionesedesign.pt/wp-content/uploads/2022/03/logo-footer.png" alt="" class="footer__logo">
             </a>
             <p class="footer__logotext">© 2022 Maionese Design</p>
             <p class="footer__logotext">Todos os direitos reservados</p>
