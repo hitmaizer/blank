acfe6b3cc24c8d43d8c0c6295720e9d71fbd86f1
Jose Alves <jose.sousalves@gmail.com>
2022-03-14 21:00:48 +0000

    error on logic function

--- functions.php
+++ functions.php
@@ -187,8 +187,21 @@ function remove_admin_bar() {
   }
 }
 
+// logout
 add_action( 'wp_logout', 'redirect_after_logout');
 function redirect_after_logout(){
   wp_redirect( home_url('/logout/') );
   exit();
+}
+
+//loginerror
+add_action( 'wp_login_failed', 'my_front_end_login_fail' );  // hook failed login
+
+function my_front_end_login_fail( $username ) {
+   $referrer = $_SERVER['HTTP_REFERER'];  // where did the post submission come from?
+   // if there's a valid referrer, and it's not the default log-in screen
+   if ( !empty($referrer) && !strstr($referrer,'wp-login') && !strstr($referrer,'wp-admin') ) {
+      wp_redirect( $referrer . '?login=failed' );  // let's append some information (login=failed) to the URL for the theme to use
+      exit;
+   }
 }
