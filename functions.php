6fd23aa8ab19b86bd70d1d9d538f729397d854f1
Jose Alves <jose.sousalves@gmail.com>
2022-03-15 16:25:13 +0000

    attempting new login attempts function

--- functions.php
+++ functions.php
@@ -223,68 +223,92 @@ function custom_authenticate_wpcc( $user, $username, $password ) {
 add_filter( 'authenticate', 'custom_authenticate_wpcc', 31, 3);
 
 //Login attempts
-function check_attempted_login( $user, $username, $password ) {
-    if ( get_transient( 'attempted_login' ) ) {
-        $datas = get_transient( 'attempted_login' );
+if ( ! class_exists( 'Limit_Login_Attempts' ) ) {
+    class Limit_Login_Attempts {
 
-        if ( $datas['tried'] >= 3 ) {
-            $until = get_option( '_transient_timeout_' . 'attempted_login' );
-            $time = time_to_go( $until );
+        var $failed_login_limit = 3;                    //Number of authentification accepted
+        var $lockout_duration   = 300;                 //Stop authentification process for 30 minutes: 60*30 = 1800
+        var $transient_name     = 'attempted_login';    //Transient used
 
-            return new WP_Error( 'too_many_tried',  sprintf( __( '<strong>ERROR</strong>: You have reached authentication limit, you will be able to try again in %1$s.' ) , $time ) );
+        public function __construct() {
+            add_filter( 'authenticate', array( $this, 'check_attempted_login' ), 30, 3 );
+            add_action( 'wp_login_failed', array( $this, 'login_failed' ), 10, 1 );
+        }
+
+        /**
+         * Lock login attempts of failed login limit is reached
+         */
+        public function check_attempted_login( $user, $username, $password ) {
+            if ( get_transient( $this->transient_name ) ) {
+                $datas = get_transient( $this->transient_name );
+
+                if ( $datas['tried'] >= $this->failed_login_limit ) {
+                    $until = get_option( '_transient_timeout_' . $this->transient_name );
+                    $time = $this->when( $until );
+
+                    //Display error message to the user when limit is reached 
+                    return new WP_Error( 'too_many_tried', sprintf( __( '<strong>ERROR</strong>: You have reached authentification limit, you will be able to try again in %1$s.' ) , $time ) );
+                }
+            }
+
+            return $user;
+        }
+
+
+        /**
+         * Add transient
+         */
+        public function login_failed( $username ) {
+            if ( get_transient( $this->transient_name ) ) {
+                $datas = get_transient( $this->transient_name );
+                $datas['tried']++;
+
+                if ( $datas['tried'] <= $this->failed_login_limit )
+                    set_transient( $this->transient_name, $datas , $this->lockout_duration );
+            } else {
+                $datas = array(
+                    'tried'     => 1
+                );
+                set_transient( $this->transient_name, $datas , $this->lockout_duration );
+            }
+        }
+
+
+        /**
+         * Return difference between 2 given dates
+         * @param  int      $time   Date as Unix timestamp
+         * @return string           Return string
+         */
+        private function when( $time ) {
+            if ( ! $time )
+                return;
+
+            $right_now = time();
+
+            $diff = abs( $right_now - $time );
+
+            $second = 1;
+            $minute = $second * 60;
+            $hour = $minute * 60;
+            $day = $hour * 24;
+
+            if ( $diff < $minute )
+                return floor( $diff / $second ) . ' secondes';
+
+            if ( $diff < $minute * 2 )
+                return "about 1 minute ago";
+
+            if ( $diff < $hour )
+                return floor( $diff / $minute ) . ' minutes';
+
+            if ( $diff < $hour * 2 )
+                return 'about 1 hour';
+
+            return floor( $diff / $hour ) . ' hours';
         }
     }
-
-    return $user;
 }
-add_filter( 'authenticate', 'check_attempted_login', 30, 3 ); 
-function login_failed( $username ) {
-    if ( get_transient( 'attempted_login' ) ) {
-        $datas = get_transient( 'attempted_login' );
-        $datas['tried']++;
 
-        if ( $datas['tried'] <= 3 )
-            set_transient( 'attempted_login', $datas , 300 );
-    } else {
-        $datas = array(
-            'tried'     => 1
-        );
-        set_transient( 'attempted_login', $datas , 300 );
-    }
-}
-add_action( 'wp_login_failed', 'login_failed', 10, 1 ); 
-
-function time_to_go($timestamp)
-{
-
-    // converting the mysql timestamp to php time
-    $periods = array(
-        "second",
-        "minute",
-        "hour",
-        "day",
-        "week",
-        "month",
-        "year"
-    );
-    $lengths = array(
-        "60",
-        "60",
-        "24",
-        "7",
-        "4.35",
-        "12"
-    );
-    $current_timestamp = time();
-    $difference = abs($current_timestamp - $timestamp);
-    for ($i = 0; $difference >= $lengths[$i] && $i < count($lengths) - 1; $i ++) {
-        $difference /= $lengths[$i];
-    }
-    $difference = round($difference);
-    if (isset($difference)) {
-        if ($difference != 1)
-            $periods[$i] .= "s";
-            $output = "$difference $periods[$i]";
-            return $output;
-    }
-}
+//Enable it:
+new Limit_Login_Attempts();
+?>
